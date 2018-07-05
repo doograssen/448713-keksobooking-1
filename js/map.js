7778dfa2dd9816fe3c8c11d4e3f8082844d0defe
Alexander <30376452+doograssen@users.noreply.github.com>
2018-07-06 00:46:12 +0300

    Перемещение маркера по карте

--- js/map.js
+++ js/map.js
@@ -14,6 +14,9 @@ var PIN_HEIGHT = 70;
 var HALF_PIN_WIDTH = 25;
 var MAIN_PIN_HEIGHT = 83;
 var MAIN_PIN_HALF_WIDTH = 32;
+var TOP_BOUNDARY = 130;
+var BOTTOM_BOUNDARY = 630;
+
 var typeMinPrice = {
   'bungalo': 0,
   'flat': 1000,
@@ -32,6 +35,7 @@ var checkinFieldElement = advertFormElement.querySelector('#timein');
 var checkoutFieldElement = advertFormElement.querySelector('#timeout');
 var roomsFieldElement = advertFormElement.querySelector('#room_number');
 var capacityFieldElement = advertFormElement.querySelector('#capacity');
+var advertAddressElement = advertFormElement.querySelector('#address');
 /* -- Вспомогательные функции --*/
 
 var getRandomValue = function (max, min) {
@@ -251,7 +255,6 @@ var onPinsMapClick = function (evt) {
 /* ---- Начальное состояние ----- */
 
 pinMainElement.addEventListener('mouseup', function (evt) {
-  var advertAddressElement = advertFormElement.querySelector('#address');
   var currentElement = evt.currentTarget;
   if (mapElement.classList.contains('map--faded')) {
     mapElement.classList.remove('map--faded');
@@ -330,23 +333,66 @@ window.addEventListener('load', function () {
 
 /* ---- Перетаскивание метки ---- */
 
+
+var getPinXCoordinate = function () {
+  return pinMainElement.offsetLeft + MAIN_PIN_HALF_WIDTH;
+};
+
+var checkPinXCoordinate = function (max, shift) {
+  var x = getPinXCoordinate() - shift;
+  return (x >= 0) && (x <= max);
+};
+
+var checkCursorXCoordinate = function (x, min, max) {
+  return (x >= min) && (x <= max);
+};
+
+var setPinXCoordinate = function (x, shift, min, max) {
+  if (checkPinXCoordinate(max, shift) && checkCursorXCoordinate(x, min, min + max)) {
+    pinMainElement.style.left = (pinMainElement.offsetLeft - shift) + 'px';
+  } else if (x < min) {
+    pinMainElement.style.left = -MAIN_PIN_HALF_WIDTH + 'px';
+  } else if (x > min + max) {
+    pinMainElement.style.left = max - MAIN_PIN_HALF_WIDTH + 'px';
+  }
+};
+
+
+var getPinYCoordinate = function () {
+  return pinMainElement.offsetTop + MAIN_PIN_HEIGHT;
+};
+
+var checkPinYCoordinate = function (min, max, shift) {
+  var y = getPinYCoordinate() - shift;
+  return (y >= min) && (y <= max);
+};
+
+var checkCursorYCoordinate = function (y, min, max) {
+  return (y >= min) && (y <= max);
+};
+
+var setPinYCoordinate = function (y, shift) {
+  if (checkPinYCoordinate(TOP_BOUNDARY, BOTTOM_BOUNDARY, shift) && checkCursorYCoordinate(y, TOP_BOUNDARY - MAIN_PIN_HEIGHT, BOTTOM_BOUNDARY)) {
+    pinMainElement.style.top = (pinMainElement.offsetTop - shift) + 'px';
+  } else if (y < TOP_BOUNDARY - MAIN_PIN_HEIGHT) {
+    pinMainElement.style.top = TOP_BOUNDARY - MAIN_PIN_HEIGHT + 'px';
+  } else if (y > BOTTOM_BOUNDARY) {
+    pinMainElement.style.top = BOTTOM_BOUNDARY - MAIN_PIN_HEIGHT + 'px';
+  }
+};
 pinMainElement.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
 
-  var startZIndex = evt.currentTarget.style.zIndex;
+  var leftBoundary = mapElement.offsetLeft;
+  var mapWidth = mapElement.clientWidth;
 
   var startCoords = {
     x: evt.clientX,
     y: evt.clientY
   };
 
-  evt.currentTarget.style.zIndex = 100;
-
-  var dragged = false;
-
   var onMouseMove = function (moveEvt) {
     moveEvt.preventDefault();
-    dragged = true;
 
     var shift = {
       x: startCoords.x - moveEvt.clientX,
@@ -357,26 +403,18 @@ pinMainElement.addEventListener('mousedown', function (evt) {
       x: moveEvt.clientX,
       y: moveEvt.clientY
     };
-
-    pinMainElement.style.top = (pinMainElement.offsetTop - shift.y) + 'px';
-    pinMainElement.style.left = (pinMainElement.offsetLeft - shift.x) + 'px';
-
+    
+    setPinYCoordinate(startCoords.y, shift.y);
+    setPinXCoordinate(startCoords.x, shift.x, leftBoundary, mapWidth);
   };
 
   var onMouseUp = function (upEvt) {
     upEvt.preventDefault();
 
+    advertAddressElement.value = getPinXCoordinate(upEvt.currentTarget) + ', ' + getPinYCoordinate(upEvt.currentTarget);
+
     document.removeEventListener('mousemove', onMouseMove);
     document.removeEventListener('mouseup', onMouseUp);
-    /*
-        if (dragged) {
-          var onClickPreventDefault = function (evt) {
-            evt.preventDefault();
-            dialogHandler.removeEventListener('click', onClickPreventDefault);
-          };
-          dialogHandler.addEventListener('click', onClickPreventDefault);
-        }*/
-
   };
 
   document.addEventListener('mousemove', onMouseMove);
